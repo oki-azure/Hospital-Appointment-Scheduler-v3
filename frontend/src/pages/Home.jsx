c8186b203f1852a29cdc9f3596b6de9ff0741d99
oki-azure <kkagboz@gmail.com>
2025-07-28 21:51:45 +0000

    .

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Header from '../components/header'
+import SpecialityMenu from '../components/SpecialityMenu'
+import TopDoctors from '../components/TopDoctors'
 
 const Home = () => {
   return (
-    <div></div>
+    <div>
+        <Header />
+        <SpecialityMenu />
+        <TopDoctors />
+    </div>
   )
 }
 

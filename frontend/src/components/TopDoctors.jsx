3b06c25770d7ea8007b8bea504f2c03f35b7fbc3
oki-azure <kkagboz@gmail.com>
2025-07-29 19:41:11 +0000

    .

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -19,8 +19,8 @@ const TopDoctors = () => {
                                 <div className='flex items-center gap-2 text-sm text-center text-green-500'>
                                     <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
                                 </div>
-                                <p>{item.name}</p>
-                                <p>{item.speciality}</p>
+                                <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
+                                <p className='text-gray-600 text-sm'>{item.speciality}</p>
                             </div>
                         </div>
                     )

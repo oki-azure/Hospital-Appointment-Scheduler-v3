7be8e361a873ad985236dde61aef6d5dab5e61eb
oki-azure <kkagboz@gmail.com>
2025-07-29 20:46:52 +0000

    .

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -10,22 +10,22 @@ const RelatedDoctors = ({speciality, docId}) => {
 
     useEffect (
         () => {
-            if (doctors.lenght > 0 && speciality) {
+            if (doctors.length > 0 && speciality) {
                 const doctorsData = doctors.filter((doc)=> doc.speciality === speciality && doc._id !== docId)
                 setRelDocs(doctorsData)
             }
         },
         [doctors, speciality, docId]
     )
-    
+
     return (
         <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
-            <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
+            <h1 className='text-3xl font-medium'>Related Doctors</h1>
             <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
             <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
                 {
                     relDoc.slice(0,5).map( (item, index)=>(
-                            <div onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+                            <div onClick={ ()=>{navigate(`/appointment/${item._id}`); scrollTo(0,0)} } className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
                                 <img className='bg-blue-50' src={item.image} alt="" />
                                 <div className='p-4'>
                                     <div className='flex items-center gap-2 text-sm text-center text-green-500'>
@@ -38,8 +38,10 @@ const RelatedDoctors = ({speciality, docId}) => {
                         )
                     )
                 }
-        </div>
-        <button onClick={()=>{navigate('/doctors'); scrollTo(0,0)}} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10 hover:scale-105 transition-all'>More</button>
+            </div>
+            <button onClick={()=>{navigate('/doctors'); scrollTo(0,0)}} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10 hover:scale-105 transition-all'>
+                More 
+            </button>
         </div>
     )
 }

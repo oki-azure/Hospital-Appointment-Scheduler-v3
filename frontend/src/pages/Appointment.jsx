c635ee3ea82ab669a1bc86617d8eb9838dc42382
oki-azure <kkagboz@gmail.com>
2025-07-29 14:20:09 +0000

    .

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Appointment = () => {
-  return (
-    <div></div>
-  )
+    const {docId} = useParams()
+    const {doctors} = useContext(AppContext)
+
+    const [docInfo, setDocInfo] = useState(null)
+
+    const fetchDocInfo = async () => {
+        const docInfo = doctors.find(doc => doc._id === docId)
+        setDocInfo(docInfo)
+        console.log(docInfo)
+    }
+
+    useEffect (
+        () => { fetchDocInfo() },
+        [doctors, docId]
+    )
+    
+    return (
+        <div>
+            {/* ---------- Doctor Details ---------- */}
+            <div>
+                <div>
+                    <img src={docInfo.image} alt="" />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Appointment

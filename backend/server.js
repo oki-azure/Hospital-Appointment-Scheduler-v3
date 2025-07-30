1def2895b5321a1cdbf3ec4974fd99fc37f503ad
oki-azure <kkagboz@gmail.com>
2025-07-30 19:16:07 +0000

    .

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express, { response } from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+
+
+// app config
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+// api endpoint
+
+app.get(
+    '/', 
+    (req,res)=> {
+        res.send('API WORKING!')
+    }
+)
+
+app.listen(port, ()=> console.log("Server Started", port))

7d56b6056e6367724f44fea6515a38849d3a668e
oki-azure <kkagboz@gmail.com>
2025-07-30 10:56:58 +0000

    started the MyAppointments page

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const MyAppointments = () => {
-  return (
-    <div></div>
-  )
+    const { doctors } = useContext(AppContext)
+
+    return (
+        <div>
+            <p>My Appointments</p>
+            <div>
+                {
+                    doctors.slice(0,2).map(
+                        (item, index) => (
+                            <div key={index}>
+
+                            </div>
+                        )
+                    )
+                }
+            </div>
+        </div>
+    )
 }
 
 export default MyAppointments

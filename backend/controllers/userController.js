7a60d5216b213cb8eda34fa0b83a1c2af3e710b5
oki-azure <kkagboz@gmail.com>
2025-08-02 09:46:46 +0000

    .

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -180,4 +180,36 @@ const listAppointments = async (req, res) => {
     }
 }
 
-export {registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointments}
+// API to cancel appointment
+const cancelAppointment = async (req, res) => {
+    try {
+        const {userId, appointmentId} = req.body
+
+        const appointmentData = await appointmentModel.findById(appointmentId)
+
+        // verify appointment user
+        if (appointmentData.userId !== userId) {
+            return res.json({success:false, message:'Unauthorized action'})
+        }
+
+        await appointmentModel.findByIdAndUpdate(appointmentId, {cancelled:true})
+
+        // Releasing doctor slot
+        const {docId, slotDate, slotTime} = appointmentData
+
+        const doctorData = await doctorModel.findById(docId)
+
+        let slots_booked = doctorData.slots_booked
+
+        slots_booked[slotDate] = slots_booked[slotDate].filter(e => e!== slotTime)
+
+        await doctorModel.findByIdAndUpdate(docId, {slots_booked})
+
+        res.json({success:true, message:'Appointment Cancelled'})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:error.message})
+    }
+}
+
+export {registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointments, cancelAppointment}
--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -6,7 +6,7 @@ import { toast } from 'react-toastify'
 import { useEffect } from 'react'
 
 const MyAppointments = () => {
-    const { backendURL, token } = useContext(AppContext)
+    const { backendURL, token, getDoctorsData } = useContext(AppContext)
 
     const [appointments, setAppointments] = useState([])
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
@@ -25,6 +25,24 @@ const MyAppointments = () => {
                 console.log(data.appointments);
                 
             }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
+
+    const cancelAppointment = async (appointmentId) => {
+        try {
+            const {data} = await axios.post(backendURL + '/api/user/cancel-appointment', {appointmentId}, {headers:{token}})
+
+            if (data.success) {
+                toast.success(data.message)
+                getUserAppointments()
+                getDoctorsData()
+            } else {
+                toast.error(data.message)
+            }
+            
         } catch (error) {
             console.log(error);
             toast.error(error.message)
@@ -61,8 +79,9 @@ const MyAppointments = () => {
                                 </div>
                                 <div></div>
                                 <div className='flex flex-col gap-2 justify-end'>
-                                    <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>
-                                    <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>
+                                    {!item.cancelled && <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>}
+                                    {!item.cancelled && <button onClick={()=>cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>}
+                                    {item.cancelled && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500'>Appointment Cancelled</button>}
                                 </div>
                             </div>
                         )

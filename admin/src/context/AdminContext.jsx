ef792a0be4fb8ec6e5590be2c3b59e84198aaf5a
oki-azure <kkagboz@gmail.com>
2025-08-02 15:02:43 +0000

    Admin Dashboard done

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -7,6 +7,8 @@ export const AdminContext = createContext()
 const AdminContextProvider = (props) => {
     const [aToken, setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : '');
     const [doctors, setDoctors] = useState([])
+    const [appointments, setAppointments] = useState([])
+    const [dashData, setDashData] = useState(false)
 
     const backendURL = import.meta.env.VITE_BACKEND_URL
 
@@ -38,11 +40,62 @@ const AdminContextProvider = (props) => {
             toast.error(error.message)
         }
     }
+
+    const getAllAppointments = async () => {
+        try {
+            const { data } = await axios.get(backendURL + '/api/admin/appointments', {headers:{aToken}})
+
+            if (data.success) {
+                setAppointments(data.appointments)
+                console.log(data.appointments);
+                
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    const cancelAppointment = async (appointmentId) => {
+        try {
+            const {data} = axios.post(backendURL + '/api/admin/cancel-appointment', {appointmentId}, {headers:{aToken}})
+
+            if (data.success) {
+                toast.success(data.message)
+                getAllAppointments()
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    const getDashData = async () => {
+        try {
+            const {data} = await axios.get(backendURL + '/api/admin/dashboard', {headers:{aToken}})
+
+            if (data.success) {
+                setDashData(data.dashData)
+                console.log(data.dashData);
+                
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
     
     const value = {
         aToken, setAToken,
         backendURL, doctors,
         getAllDoctors, changeAvailability,
+        appointments, setAppointments,
+        getAllAppointments,
+        cancelAppointment,
+        dashData, getDashData
     }
 
     return (
--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -3,11 +3,10 @@ import bcrypt from 'bcrypt'
 import { v2 as cloudinary } from 'cloudinary'
 import doctorModel from "../models/doctorModel.js"
 import jwt from 'jsonwebtoken'
+import appointmentModel from "../models/appointmentModel.js"
+import userModel from "../models/userModel.js"
 
 // API for adding doctor
-
-import { request } from "express"
-
 const addDoctor = async (req, res) => {
     try {
         const { name, email, password, speciality, degree, experience, about, fees, address } = req.body 
@@ -60,7 +59,7 @@ const addDoctor = async (req, res) => {
     }
 }
 
-//API for admin login
+// API for admin login
 const loginAdmin = async (req,res) => {
     try {
         
@@ -91,4 +90,63 @@ const allDoctors = async (req, res) => {
     }
 }
 
-export {addDoctor, loginAdmin, allDoctors}
+// API to get all appointments list
+const appointmentsAdmin = async (req, res) => {
+    try {
+        const appointments = await appointmentModel.find({})
+        res.json({success:true, appointments})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:error.message})
+    }
+}
+
+// API for appointment cancellation by admin
+const appointmentCancel = async (req, res) => {
+    try {
+        const {appointmentId} = req.body
+
+        const appointmentData = await appointmentModel.findById(appointmentId)
+
+
+        await appointmentModel.findByIdAndUpdate(appointmentId, {cancelled:true})
+
+        // Releasing doctor slot
+        const {docId, slotDate, slotTime} = appointmentData
+
+        const doctorData = await doctorModel.findById(docId)
+
+        let slots_booked = doctorData.slots_booked
+
+        slots_booked[slotDate] = slots_booked[slotDate].filter(e => e!== slotTime)
+
+        await doctorModel.findByIdAndUpdate(docId, {slots_booked})
+
+        res.json({success:true, message:'Appointment Cancelled'})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:error.message})
+    }
+}
+
+// API to get dashboard data for admin panel
+const adminDashboard = async (req, res) => {
+    try {
+        const doctors = await doctorModel.find({})
+        const users = await userModel.find({})
+        const appointments = await appointmentModel.find({})
+
+        const dashData = {
+            doctors: doctors.length,
+            appointments: appointments.length,
+            patients: users.length,
+            latestAppointments: appointments.reverse().slice(0,5)
+        }
+
+        res.json({success:true, dashData})
+    } catch (error) {
+        
+    }
+}
+
+export {addDoctor, loginAdmin, allDoctors, appointmentsAdmin, appointmentCancel, adminDashboard}
